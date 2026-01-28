a87020942b5e8219012764659ba6e1691c9306b7
rahimkpm <pmvkatp@gmail.com>
2026-01-28 22:39:21 +0530

    final

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { HttpClient } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { provideToastr, ToastrModule } from 'ngx-toastr';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),HttpClient,
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,withHashLocation()),HttpClient,
     provideHttpClient(withInterceptors([spinnerInterceptor])),provideAnimations(), provideToastr()
     ],
 };
