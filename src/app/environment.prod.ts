0263f05862505628b645724a93c3aa96bc2cb85a
rahimkpm <pmvkatp@gmail.com>
2026-01-30 16:54:53 +0530

    dd

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  //apiBaseUrl:'https://localhost:5219/api'
   apiBaseUrl: 'https://rahimkhan-001-site1.rtempurl.com/api'
 };
--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://rahimkhan-001-site1.rtempurl.com/api'
+  //apiBaseUrl:'https://localhost:5219/api'
+   apiBaseUrl: 'https://rahimkhan-001-site1.rtempurl.com/api'
 };
